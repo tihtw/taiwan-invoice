53ef0e61490162a1cae3f6475ac11ffe35688172
Pichu Chen <pichu@tih.tw>
2020-08-13 19:05:58 +0800

    add love code

---
+++ example/loveCodeVerify.go
@@ -0,0 +1,32 @@
+package main
+
+// 手機條碼驗證
+
+import (
+	"fmt"
+	"github.com/tihtw/taiwan-invoice"
+	"os"
+)
+
+func main() {
+
+	appId := os.Getenv("PICHU_EINVOICE_APPID")
+	if appId == "" {
+		fmt.Println("Please use export PICHU_EINVOICE_APPID={your_app_id} for setting appId")
+		return
+	}
+
+	host := os.Getenv("INVOICE_CONNECTION_HOST")
+
+	c := invoice.NewClient(appId)
+
+	if host != "" {
+		c.ConnectionHost = host
+	}
+	isExist, err := c.CheckLoveCodeExist("1989")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("is exist:", isExist)
+}
